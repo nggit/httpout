a2adf4bc86c5c9e61b09cc5c792cf0bf9c8a29d7
nggit <contact@anggit.com>
2024-10-03 17:31:28 +0700

    clean up setup.py (#20)
    * clean up setup.py
    
    * fix recursive error
    
    ---------
    
    Co-authored-by: nggit <12218311+nggit@users.noreply.github.com>

--- httpout/utils/_modules.py
+++ httpout/utils/_modules.py
@@ -10,7 +10,7 @@ def cleanup_modules(modules, excludes=()):
                 if value in excludes or name.startswith('__'):
                     continue
 
-                if (hasattr(value, '__dict__') and
+                if (value is not module and hasattr(value, '__dict__') and
                         not isinstance(value, (type, ModuleType))):
                     cleanup_modules(value.__dict__, excludes)
 
