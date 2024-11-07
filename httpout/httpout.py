9dd684609219dc5dfcc2ae9431a523f680b1cfbf
nggit <12218311+nggit@users.noreply.github.com>
2024-11-07 22:05:49 +0700

    refactor __globals__

--- httpout/httpout.py
+++ httpout/httpout.py
@@ -31,7 +31,6 @@ class HTTPOut:
 
     async def _on_worker_start(self, **worker):
         worker_ctx = worker['context']
-        app = worker['app']
         loop = worker['loop']
         logger = worker['logger']
         thread_pool_size = worker_ctx.options.get('thread_pool_size', 5)
@@ -40,29 +39,34 @@ class HTTPOut:
         )
         worker_ctx.options['document_root'] = document_root
 
+        logger.info('entering directory: %s', document_root)
+        os.chdir(document_root)
+        sys.path.insert(0, document_root)
+
+        # provides __globals__, a worker-level context
+        worker['__globals__'] = new_module('__globals__')
+
         def wait(coro, timeout=None):
             return asyncio.run_coroutine_threadsafe(coro, loop).result(timeout)
 
         def load_module(name, globals, level=0):
-            if name in globals['__main__'].__server__['modules']:
+            if (globals['__name__'] != '__globals__' and
+                    name in globals['__main__'].__server__['modules']):
                 # already imported
                 return globals['__main__'].__server__['modules'][name]
 
             module = new_module(name, level, document_root)
 
             if module:
-                logger.info(
-                    '%d: %s: importing %s',
-                    globals['__main__'].__server__['request'].socket.fileno(),
-                    globals['__name__'],
-                    name
-                )
-                module.__main__ = globals['__main__']
-                module.__server__ = globals['__main__'].__server__
-                module.print = globals['__main__'].print
-                module.run = globals['__main__'].run
-                module.wait = wait
-                globals['__main__'].__server__['modules'][name] = module
+                logger.info('%s: importing %s', globals['__name__'], name)
+
+                if globals['__name__'] != '__globals__':
+                    module.__main__ = globals['__main__']
+                    module.__server__ = globals['__main__'].__server__
+                    module.print = globals['__main__'].print
+                    module.run = globals['__main__'].run
+                    module.wait = wait
+                    globals['__main__'].__server__['modules'][name] = module
 
                 exec_module(module)
                 return module
@@ -71,16 +75,15 @@ class HTTPOut:
 
         def ho_import(name, globals=None, locals=None, fromlist=(), level=0):
             if (name not in sys.builtin_module_names and
-                    globals is not None and '__main__' in globals and
-                    globals['__main__'].__file__.startswith(document_root)):
+                    globals is not None and '__file__' in globals and
+                    globals['__file__'].startswith(document_root)):
                 # satisfy import __main__
                 if name == '__main__':
-                    logger.info(
-                        '%d: %s: importing __main__',
-                        globals['__main__'].__server__['request']
-                                           .socket.fileno(),
-                        globals['__name__']
-                    )
+                    logger.info('%s: importing __main__', globals['__name__'])
+
+                    if globals['__name__'] == '__globals__':
+                        return worker['__globals__']
+
                     return globals['__main__']
 
                 oldname = name
@@ -104,9 +107,12 @@ class HTTPOut:
                     return module
 
                 if name == 'httpout' or name.startswith('httpout.'):
-                    module = globals['__main__'].__server__['modules'][
-                        globals['__name__']
-                    ]
+                    if globals['__name__'] == '__globals__':
+                        module = worker['__globals__']
+                    else:
+                        module = globals['__main__'].__server__['modules'][
+                            globals['__name__']
+                        ]
 
                     # handles virtual imports,
                     # e.g. from httpout import request, response
@@ -115,20 +121,19 @@ class HTTPOut:
                             if child in module.__dict__:
                                 continue
 
-                            if child in module.__server__:
-                                module.__dict__[child] = module.__server__[
-                                    child
-                                ]
-                            else:
+                            if (globals['__name__'] == '__globals__' or
+                                    child not in module.__server__):
                                 try:
-                                    module.__dict__[child] = getattr(
-                                        builtins, child
-                                    )
-                                except AttributeError as exc:
+                                    module.__dict__[child] = worker[child]
+                                except KeyError as exc:
                                     raise ImportError(
                                         f'cannot import name \'{child}\' '
                                         f'from \'{name}\''
                                     ) from exc
+                            else:
+                                module.__dict__[child] = module.__server__[
+                                    child
+                                ]
 
                     return module
 
@@ -139,38 +144,17 @@ class HTTPOut:
         worker_ctx.wait = wait
         worker_ctx.caches = {}
         worker_ctx.executor = MultiThreadExecutor(thread_pool_size)
-
         worker_ctx.executor.start()
-        logger.info('entering directory: %s', document_root)
-        os.chdir(document_root)
-        sys.path.insert(0, document_root)
 
-        # provides __globals__, a worker-level context
-        builtins.__globals__ = new_module('__globals__')
-        app.ctx = worker_ctx
+        if worker['__globals__']:
+            exec_module(worker['__globals__'])
 
-        if __globals__:  # noqa: F821
-            exec_module(__globals__)  # noqa: F821
-
-            if '__enter__' in __globals__.__dict__:  # noqa: F821
-                coro = __globals__.__enter__(app)  # noqa: F821
-
-                if hasattr(coro, '__await__'):
-                    await coro
+            builtins.__globals__ = worker['__globals__']
         else:
             builtins.__globals__ = ModuleType('__globals__')
 
     async def _on_worker_stop(self, **worker):
-        app = worker['app']
-
-        try:
-            if '__exit__' in __globals__.__dict__:  # noqa: F821
-                coro = __globals__.__exit__(app)  # noqa: F821
-
-                if hasattr(coro, '__await__'):
-                    await coro
-        finally:
-            await app.ctx.executor.shutdown()
+        await worker['context'].executor.shutdown()
 
     async def _on_request(self, **server):
         request = server['request']
@@ -226,10 +210,8 @@ class HTTPOut:
 
         if ext == '.py':
             # begin loading the module
-            logger.info(
-                '%d: %s -> __main__: %s',
-                request.socket.fileno(), path, module_path
-            )
+            logger.info('%s -> __main__: %s', path, module_path)
+
             server['request'] = HTTPRequest(request, server)
             server['response'] = HTTPResponse(response)
             server['REQUEST_METHOD'] = request.method.decode('latin-1')
@@ -264,9 +246,7 @@ class HTTPOut:
             code = worker_ctx.caches.get(module_path, None)
 
             if code:
-                logger.info(
-                    '%d: %s: using cache', request.socket.fileno(), path
-                )
+                logger.info('%s: using cache', path)
 
             try:
                 # execute module in another thread
@@ -277,9 +257,7 @@ class HTTPOut:
 
                 if result:
                     worker_ctx.caches[module_path] = result
-                    logger.info(
-                        '%d: %s: cached', request.socket.fileno(), path
-                    )
+                    logger.info('%s: cached', path)
                 else:
                     # cache is going to be deleted on @app.on_close
                     # but it can be delayed on a Keep-Alive request
@@ -330,10 +308,7 @@ class HTTPOut:
         if ext not in mime_types:
             raise Forbidden(f'Disallowed file extension: {ext}')
 
-        logger.info(
-            '%d: %s -> %s: %s',
-            request.socket.fileno(), path, mime_types[ext], module_path
-        )
+        logger.info('%s -> %s: %s', path, mime_types[ext], module_path)
         await response.sendfile(
             module_path,
             content_type=mime_types[ext],
