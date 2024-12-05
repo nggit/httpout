ea834482952ac396595ce08480ab808cae9b4ba6
nggit <contact@anggit.com>
2024-12-05 10:45:26 +0700

    release 0.0.66 (#58)
    * add httpout.exceptions
    
    ---------
    
    Co-authored-by: nggit <12218311+nggit@users.noreply.github.com>

--- httpout/__init__.py
+++ httpout/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2024 nggit
 
-__version__ = '0.0.65'
+__version__ = '0.0.66'
 __all__ = ('HTTPOut',)
 
 from .httpout import HTTPOut  # noqa: E402
---
+++ httpout/exceptions.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2024 nggit
+
+from tremolo.exceptions import (  # noqa: F401
+    WebSocketException,
+    WebSocketClientClosed,
+    WebSocketServerClosed
+)
